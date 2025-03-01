bdd0620e59a5a03803542f3bbd6b233fa2092b3c
JingyangXiang <jingyangxiang@zju.edu.cn>
2025-03-01 16:25:10 +0800

    update rotart_emb for new transformers

--- utils/eval_utils.py
+++ utils/eval_utils.py
@@ -26,6 +26,8 @@ def evaluator(model, testenc, dev, args):
 
     layers = model.model.layers
     model.model.embed_tokens = model.model.embed_tokens.to(dev)
+    if hasatter(model.model, "rotary_emb"):
+        model.model.rotary_emb = model.model.rotary_emb.to(dev)
 
     layers[0] = layers[0].to(dev)
 
